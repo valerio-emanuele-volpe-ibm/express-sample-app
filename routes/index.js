e6968801aff325dc827d199d1681fb34b0d73335
Valerio E. Volpe <valerio.emanuele.volpe@ibm.com>
2021-06-21 13:19:49 +0200

    Personalize app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Valerio\'s Express App' });
 });
 
 module.exports = router;
